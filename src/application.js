4926619dbc8d5be952e04bb0da5555ebcd4b05fa
mortal pjero <mortalpjero@gmail.com>
2023-08-16 14:05:48 +0100

    fix: fixed linting and refactored the code

--- src/application.js
+++ src/application.js
@@ -12,12 +12,11 @@ const schema = yup.object().shape({
 // Логика поиска ошибок
 
 const validateLink = (fields) => {
-  try {
-    schema.validateSync(fields, { abortEarly: false });
-    return {};
-  } catch (e) {
-    return _.keyBy(e.inner, 'path');
-  }
+  const validation = schema
+    .validate(fields, { abortEarly: false })
+    .then(() => { })
+    .catch((e) => _.keyBy(e.inner, 'path'));
+  return validation;
 };
 
 // Логика валидации ссылки
@@ -35,28 +34,36 @@ const app = () => {
   // Изменения state
 
   const handleLinkValidation = (watchedState) => {
-    const validateLinkResult = validateLink(watchedState.link);
-    if (!_.isEmpty(validateLinkResult)) {
-      watchedState.link.status = 'invalid';
-      watchedState.link.error = validateLinkResult.linkContent.message;
-    } else if (watchedState.link.existingLinks.includes(watchedState.link.linkContent)) {
-      watchedState.link.status = 'invalid';
-      watchedState.link.error = 'RSS уже существует';
-    } else {
-      watchedState.link.status = 'valid';
-      watchedState.link.error = '';
-      watchedState.link.existingLinks.push(watchedState.link.linkContent);
-    }
-    view(watchedState);
+    validateLink(watchedState.link).then((validateLinkResult) => {
+      const updatedWatchedState = { ...watchedState };
+
+      // Если ошибки есть, то форма становится невалдиной
+      // Если в существующий ссылках есть ссылка, то появляется ошибка
+      // В остальных случаях форма валидна
+      if (!_.isEmpty(validateLinkResult)) {
+        updatedWatchedState.link.status = 'invalid';
+        updatedWatchedState.link.error = validateLinkResult.linkContent.message;
+      } else if (
+        updatedWatchedState.link.existingLinks.includes(updatedWatchedState.link.linkContent)
+      ) {
+        updatedWatchedState.link.status = 'invalid';
+        updatedWatchedState.link.error = 'RSS уже существует';
+      } else {
+        updatedWatchedState.link.status = 'valid';
+        updatedWatchedState.link.error = '';
+        updatedWatchedState.link.existingLinks.push(updatedWatchedState.link.linkContent);
+      }
+      view(updatedWatchedState);
+    });
   };
 
   // Логика контроллера
 
-  const watchedState = onChange(state, path => {
+  const watchedState = onChange(state, (path) => {
     if (path === 'link.status' && watchedState.link.status === 'validation') {
       handleLinkValidation(watchedState);
     }
-  })
+  });
 
   const linkInput = document.querySelector('#url-input');
   const form = document.querySelector('.rss-form');
@@ -67,9 +74,9 @@ const app = () => {
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    watchedState.link.status = 'validation'
+    watchedState.link.status = 'validation';
     form.reset();
-  })
+  });
 };
 
 export default app;
