fc72713efda86deacab83af85d06c58d29f0b39f
mortal pjero <mortalpjero@gmail.com>
2023-08-24 16:26:40 +0100

    fix: fixed webpack warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack'); // Import webpack module
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -23,8 +22,7 @@ const config = {
         }),
 
         new MiniCssExtractPlugin(),
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new webpack.NoEmitOnErrorsPlugin(), // Suppress errors
     ],
     module: {
         rules: [
@@ -34,7 +32,23 @@ const config = {
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
+                use: [
+                    stylesHandler,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                quietDeps: true, // Suppress Sass warnings
+                            },
+                        },
+                    },
+                ],
             },
             {
                 test: /\.css$/i,
@@ -44,20 +58,9 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
+    mode: isProduction ? 'production' : 'development',
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-
-
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+module.exports = config;
